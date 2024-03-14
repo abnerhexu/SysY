fa6408b771e5c6fc7e5f6f7fa470f4d4c32462f9
abnerhexu <hexu@rochexp.com>
2024-03-14 20:57:12 +0800

    bug fixed for irbuilder.h

--- src/IRBuilder.h
+++ src/IRBuilder.h
@@ -52,7 +52,7 @@ public:
                            name);
   }
   UnaryInst *createIToFInst(Value *operand, const std::string &name = "") {
-    return createUnaryInst(Instruction::kIToF, Type::getFloatType(), operand,
+    return createUnaryInst(Instruction::kItoF, Type::getFloatType(), operand,
                            name);
   }
   BinaryInst *createBinaryInst(Instruction::Kind kind, Type *type, Value *lhs,
