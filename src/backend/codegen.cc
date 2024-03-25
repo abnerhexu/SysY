8ca1cb96f5a99ee1f2d28f7540398a2735f0f379
abnerhexu <hexu@rochexp.com>
2024-03-25 22:12:06 +0800

    add backend

--- src/backend/codegen.cc
+++ src/backend/codegen.cc
@@ -14,6 +14,7 @@ std::string CodeGen::module_gen(sysy::Module *module) {
   std::string dataCode;
   std::string textCode;
   std::string compilerIdentifier = "SysY compiler";
+  std::string assemblyCode;
 
   clearModuleLabels(module);
 
@@ -22,8 +23,172 @@ std::string CodeGen::module_gen(sysy::Module *module) {
   descriptionCode += space + ".text" + endl;
   descriptionCode += space + ".globl main" + endl;
   dataCode += globalData_gen(); 
-  
-   
+  std::map<std::string, sysy::Function*> *funcs = module->getFunctions();
+  for (auto it = funcs->begin(); it != funcs->end(); it++) {
+    std::string fname = it->first;
+    sysy::Function *func = it->second;
+    auto bbs = func->getBasicBlocks();
+    if (bbs.empty()) {
+      continue;
+    }
+    textCode += function_gen(func) + endl;
+  }
+
+  assemblyCode += (descriptionCode + dataCode + textCode + endl);
+  return assemblyCode;
 }
 
+std::string CodeGen::functionHeader_gen(sysy::Function *func){
+  std::string assemblyCode;
+  assemblyCode += space + ".global " + func->getName() + endl;
+  assemblyCode += space + ".type " + func->getName() + ", @function" + endl;
+  return assemblyCode;
+}
+
+std::string CodeGen::CalleeRegSave_gen(sysy::Function* func){
+  std::string assemblyCode;
+  return assemblyCode;
+}
+
+std::string CodeGen::CalleeRegRestore_gen(sysy::Function* func){
+  std::string assemblyCode;
+  return assemblyCode;
+}
+
+std::string CodeGen::function_gen(sysy::Function *func) {
+  this->curFunc = func;
+  clearFuncInfo(func);
+  std::string bbCode;
+  auto bbs = func->getBasicBlocks();
+  for (auto it = bbs.begin(); it != bbs.end(); it++){
+    auto bb = it->get();
+    bbCode = basicBlock_gen(bb);
+  }
+  std::string assemblyCode;
+  std::string headerCode = functionHeader_gen(func);
+  std::string calleeRegSaveCode = CalleeRegSave_gen(func);
+  std::string calleeRegRestoreCode = CalleeRegRestore_gen(func);
+
+  // TODO literalPoolsCode
+  assemblyCode = headerCode + calleeRegSaveCode + bbCode + calleeRegRestoreCode;
+
+  return assemblyCode;
+}
+
+std::string CodeGen::basicBlock_gen(sysy::BasicBlock *bb) {
+  this->curBBlock = bb;
+  std::string bbLabel = this->getBasicBlocksLabel(bb);
+  std::string assemblyCode;
+  assemblyCode += bbLabel + ":" + endl;
+  for (auto &inst: bb->getInstructions()) {
+    auto instType = inst->getKind();
+    assemblyCode += instruction_gen(inst.get());
+  }
+  return assemblyCode;
+}
+
+std::pair<int, std::string> CodeGen::GenBinaryInst(sysy::BinaryInst *inst) {
+  std::string instruction;
+
+}
+
+std::pair<int, std::string> CodeGen::GenAllocaInst(sysy::AllocaInst *inst) {
+  std::string instruction;
+  int destRegID = regManager.requestReg(RegisterManager::RegType::IntReg, RegisterManager::RegHint::sp);
+  for (auto &var: regManager.varIRegMap){
+    if (var.second.first == RegisterManager::VarPos::OnStack) {
+      var.second.second += inst->getType()->getSize();
+    }
+  }
+  regManager.varIRegMap.insert({inst->getName(), {RegisterManager::VarPos::OnStack, 0}});
+  instruction = "addi sp, sp, " + std::to_string(inst->getType()->getSize()) + endl;
+  return {destRegID, instruction};
+}
+
+std::string CodeGen::GenStoreInst(sysy::StoreInst *inst) {
+  std::string instruction;
+  auto src = inst->getValue();
+  auto constSrc = dynamic_cast<sysy::ConstantValue *>(src);
+  auto destName = inst->getPointer()->getName();
+  auto destPos = regManager.varIRegMap.find(destName);
+  assert(destPos != regManager.varIRegMap.end());
+  if (constSrc) {
+    if (constSrc->isInt()) {
+      int tmpRegID = regManager.requestReg(RegisterManager::RegType::IntReg, RegisterManager::RegHint::temp);
+      instruction += "addi " + regManager.intRegs[tmpRegID].second + ", " + "zero, " + std::to_string(constSrc->getInt()) + endl;
+      if (destPos->second.first == RegisterManager::VarPos::OnStack) {
+        instruction += "sw " + regManager.intRegs[tmpRegID].second + ", " + std::to_string(destPos->second.second) + "(" + "sp" + ")" + endl;
+      }
+      else {
+        std::cerr << "do not suppport float imm-mem store at this moment" << std::endl;
+        exit(1);
+      }
+      assert(regManager.releaseReg(RegisterManager::RegType::IntReg, tmpRegID)); // in case of release failed
+    }
+  }
+  else {
+    // var in registers
+    if (src->getType()->isInt()) {
+      auto srcReg = regManager.varIRegMap.find(src->getName());
+      if (srcReg != regManager.varIRegMap.end()) {
+        if (srcReg->second.first == RegisterManager::VarPos::InReg) {
+          if (destPos->second.first == RegisterManager::VarPos::OnStack){
+            instruction += "sw " + regManager.intRegs[srcReg->second.second].second + ", " + std::to_string(destPos->second.second) + "(sp)" + endl;
+          }
+          else {
+            std::cerr << "do not suppport non on-stack reg-mem store at this moment" << std::endl;
+            exit(1);
+          }
+        }
+        else {
+          std::cerr << "Error: store to stack" << std::endl;
+          exit(1);
+        }
+      }
+      else {
+        std::cerr << "Error: cannot find src reg" << std::endl;
+        exit(1);
+      }
+    }
+    else {
+      // float reg
+      std::cerr << "do not support float reg-mem store at this moment" << std::endl;
+      exit(1);
+    }
+  }
+  return instruction;
+}
+
+std::string CodeGen::instruction_gen(sysy::Instruction *inst) {
+  std::string instruction;
+  std::string instName = inst->getName();
+  auto instType = inst->getKind();
+  std::pair<int, std::string> instInfo;
+  switch (instType) {
+    case sysy::Instruction::kAdd:
+    case sysy::Instruction::kSub:
+    case sysy::Instruction::kMul:
+    case sysy::Instruction::kDiv: {
+      sysy::BinaryInst *bInst = dynamic_cast<sysy::BinaryInst *>(inst);
+      instInfo = GenBinaryInst(bInst);
+    }
+  }
+}
+
+int RegisterManager::requestReg(RegType rtype, RegHint hint) {
+  switch (hint) {
+    case zero:
+      return 0;
+    case ra:
+      return 1;
+    case sp:
+      return 2;
+    case gp:
+      return 3;
+    case tp:
+      return 4;
+    case t0:
+    
+  }
+}
 }
--- src/backend/codegen.h
+++ src/backend/codegen.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include "IR.h"
@@ -12,13 +13,71 @@ static std::string RTypeInst(std::string name, std::string rd, std::string rs1, 
 
 
 class RegisterManager {
+  friend class CodeGen;
 public:
   std::vector<std::pair<std::string, std::string>> intRegs = {{"x0", "zero"}, {"x1", "ra"}, {"x2", "sp"}, {"x3", "gp"}, {"x4", "tp"}, {"x5", "t0"}, {"x6", "t1"}, {"x7", "t2"}, {"x8", "s0"}, {"x9", "s1"}, {"x10", "a0"}, {"x11", "a1"}, {"x12", "a2"}, {"x13", "a3"}, {"x14", "a4"}, {"x15", "a5"}, {"x16", "a6"}, {"x17", "a7"}, {"x18", "s2"}, {"x19", "s3"}, {"x20", "s4"}, {"x21", "s5"}, 
   {"x22", "s6"}, {"x23", "s7"}, {"x24", "s8"}, {"x25", "s9"}, {"x26", "s10"}, {"x27", "s11"}, {"x28", "t3"}, {"x29", "t4"}, {"x30", "t5"}, {"x31", "t6"}};
 
   std::vector<std::pair<std::string, std::string>> floatRegs = {{"f0", "ft0"}, {"f1", "ft1"}, {"f2", "ft2"}, {"f3", "ft3"}, {"f4", "ft4"}, {"f5", "ft5"}, {"f6", "ft6"}, {"f7", "ft7"}, {"f8", "fs0"}, {"f9", "fs1"}, {"f10", "fa0"}, {"f11", "fa1"}, {"f12", "fa2"}, {"f13", "fa3"}, {"f14", "fa4"}, {"f15", "fa5"}, {"f16", "fa6"}, {"f17", "fa7"}, {"f18", "fs2"}, {"f19", "fs3"}, {"f20", "fs4"}, {"f21", "fs5"}, {"f22", "fs6"},
   {"f23", "fs7"}, {"f24", "fs8"}, {"f25", "fs9"}, {"f26", "fs10"}, {"f27", "fs11"}, {"f28", "ft8"}, {"f29", "ft9"}, {"f30", "ft10"}, {"f31", "ft11"}};
+
+  enum VarPos {
+    Globals, OnStack, InReg
+  };
+
+  std::map<std::string, std::pair<VarPos, int>> varIRegMap;
+  std::map<std::string, std::pair<VarPos, int>> varFRegMap;
+
+private:
+  std::vector<bool> intRegTaken;
+  std::vector<bool> floatRegTaken;
+
+public:
+  enum RegType {
+    IntReg, FloatReg
+  };
+
+  enum RegHint {
+    dontCare,
+    zero,
+    ra,
+    sp,
+    gp,
+    tp,
+    temp,
+    fp,
+    arg,
+    saved
+  };
+
+public:
+  int requestReg(RegType rtype, RegHint hint = dontCare);
+  bool releaseReg(RegType rtype, int regID);
+
+  void resetRegs(RegType rtype){
+    switch (rtype) {
+    case IntReg:
+      this->intRegTaken.clear();
+      this->intRegTaken.resize(32, false);
+      break;
+    case FloatReg:
+      this->floatRegTaken.clear();
+      this->floatRegTaken.resize(32, false);
+      break;
+    default:
+      std::cerr << "Unknown register type" << std::endl;
+      exit(1);
+    }
+  }
+
+public:
+  RegisterManager() {
+    this->resetRegs(IntReg);
+    this->resetRegs(FloatReg);
+  }
 }; // class RegisterManager
+
+RegisterManager regManager;
 
 class InstOperand {
 public:
@@ -76,7 +135,9 @@ public:
       exit(1);
     }
   }
-};
+}; // class InstOperand
+
+
 
 class CodeGen {
 public:
@@ -116,10 +177,10 @@ public:
   std::string basicBlock_gen(sysy::BasicBlock* bblock);
   std::string instruction_gen(sysy::Instruction* inst);
   std::string globalData_gen();
-  std::string prologueCode_gen(sysy::Function *func);
-  std::string epilogueCode_gen(sysy::Function *func);
+  std::string CalleeRegSave_gen(sysy::Function *func);
+  std::string CalleeRegRestore_gen(sysy::Function *func);
   std::string literalPoolsCode_gen(sysy::Function *func);
-  std::string functionHead_gen(sysy::Function *func);
+  std::string functionHeader_gen(sysy::Function *func);
 
   void clearModuleLabels(sysy::Module *module) {
     this->bblabelId = 0;
@@ -128,10 +189,10 @@ public:
   // instruction generator
   std::pair<int, std::string> GenLoadInst(sysy::LoadInst* inst, int dstRegID);
   std::string GenStoreInst(sysy::StoreInst* inst);
-  std::pair<int, std::string> GenAllocaInst(sysy::AllocaInst* inst, int dstRegID);
+  std::pair<int, std::string> GenAllocaInst(sysy::AllocaInst* inst);
   std::string GenRetuenInst(sysy::ReturnInst* inst);
   std::pair<int, std::string> GenCallInst(sysy::CallInst* inst, int dstRegID);
-  std::pair<int, std::string> GenBinaryInst(sysy::BinaryInst* inst, int dstRegID);
+  std::pair<int, std::string> GenBinaryInst(sysy::BinaryInst* inst);
   std::pair<int, std::string> GenUnaryInst(sysy::UnaryInst* inst, int dstRegID);
   std::string GenUncondBrInst(sysy::UncondBrInst* inst);
   std::string GenCondBrInst(sysy::CondBrInst* inst);
