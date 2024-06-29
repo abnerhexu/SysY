2d461533ecd765b3a8c7c92d3ca6a113d0a4d9cc
2317891476 <2317891476@qq.com>
2024-06-30 02:31:23 +0800

    v4_add,sub,mul,div,sw,lw,

--- src/opt/constprop.cc
+++ src/opt/constprop.cc
@@ -1,6 +1,7 @@
 #include "constprop.h"
 #include "../frontend/IR.h"
 #include <unordered_map>
+#include <string>
 
 namespace transform {
 
@@ -19,21 +20,102 @@ void ConstProp::moduleTransform() {
 }
 
 void ConstProp::constantPropagation(sysy::BasicBlock *bb) {
+    int flag;
     std::unordered_map<std::string, std::string> valueMap;
+    std::unordered_map<std::string, std::string> valuechange;
     for (auto &inst : bb->CoInst) {
         if (inst.op == "li") { // Assuming 'li' is the instruction to load immediate constants.
             valueMap[inst.fields[0]] = inst.fields[1]; // Store constant value with its register.
+            valuechange[inst.fields[0]] = "0";
         }
-        else if(inst.op == "add" || inst.op == "mul" || inst.op == "sub" ||  inst.op == "div" ){
-            //int len = inst.fields.size();
+        else if(inst.op == "add" ){
             for (int i=1;i<=2;i++){
-                if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[i]) != valueMap.end()) {
-                // Replace uses of constants.
+                flag = false;
+                if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[i]) != valueMap.end() && valuechange[inst.fields[i]] == "0") {
                     inst.fields[i] = valueMap[inst.fields[i]];
+                    flag = true;
                 }
+                else break;
             }
-            //int temp1 = (int*) valueMap[inst.fields[1]];
-            //valueMap[inst.fields[0]] = valueMap[inst.fields[1]] + valueMap[inst.fields[2]];
+            if (flag){
+                int temp1 =  std::stoll(inst.fields[1]);
+                int temp2 =  std::stoll(inst.fields[2]);
+                valueMap[inst.fields[0]] = std::to_string(temp1 + temp2);
+            }
+            else{
+                valuechange[inst.fields[0]] = "1";
+            }
+        }
+        else if(inst.op == "sub" ){
+            for (int i=1;i<=2;i++){
+                flag = false;
+                if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[i]) != valueMap.end() && valuechange[inst.fields[i]] == "0") {
+                    inst.fields[i] = valueMap[inst.fields[i]];
+                    flag = true;
+                }
+                else break;
+            }
+            if (flag){
+                int temp1 =  std::stoll(inst.fields[1]);
+                int temp2 =  std::stoll(inst.fields[2]);
+                valueMap[inst.fields[0]] = std::to_string(temp1 - temp2);
+            }
+            else{
+                valuechange[inst.fields[0]] = "1";
+            }
+        }
+        else if(inst.op == "mul" ){
+            for (int i=1;i<=2;i++){
+                flag = false;
+                if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[i]) != valueMap.end() && valuechange[inst.fields[i]] == "0") {
+                    inst.fields[i] = valueMap[inst.fields[i]];
+                    flag = true;
+                }
+                else break;
+            }
+            if (flag){
+                int temp1 =  std::stoll(inst.fields[1]);
+                int temp2 =  std::stoll(inst.fields[2]);
+                valueMap[inst.fields[0]] = std::to_string(temp1 * temp2);
+            }
+            else{
+                valuechange[inst.fields[0]] = "1";
+            }
+        }
+        else if(inst.op == "div" ){
+            for (int i=1;i<=2;i++){
+                flag = false;
+                if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[i]) != valueMap.end() && valuechange[inst.fields[i]] == "0") {
+                    inst.fields[i] = valueMap[inst.fields[i]];
+                    flag = true;
+                }
+                else break;
+            }
+            if (flag){
+                int temp1 =  std::stoll(inst.fields[1]);
+                int temp2 =  std::stoll(inst.fields[2]);
+                valueMap[inst.fields[0]] = std::to_string(temp1 / temp2);
+            }
+            else{
+                valuechange[inst.fields[0]] = "1";
+            }
+        }
+        else if (inst.op == "sw" ){ 
+            auto nextInst = std::next(&inst);
+            if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[0]) != valueMap.end() && valuechange[inst.fields[0]] == "0") {
+                // Replace uses of constants.
+                    inst.fields[0] = valueMap[inst.fields[0]];
+            }
+            if (nextInst->op == "lw"){
+                valueMap[nextInst->fields[0]] = inst.fields[0];// Store constant value with its register.
+                valuechange[nextInst->fields[0]] = "0";
+            }
+        }
+        else if (inst.op == "lw" ){ 
+            if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[0]) != valueMap.end() && valuechange[inst.fields[0]] == "0") {
+                // Replace uses of constants.
+                    inst.fields[0] = valueMap[inst.fields[0]];
+            }
         }
         else{
            continue;
