33f9f0bc3ee2f65bbac37f76dbc955c05e69627b
2317891476 <2317891476@qq.com>
2024-06-29 22:57:45 +0800

    bug_fix_low_perform

--- src/opt/constprop.cc
+++ src/opt/constprop.cc
@@ -20,12 +20,18 @@ void ConstProp::moduleTransform() {
 
 void ConstProp::constantPropagation(sysy::BasicBlock *bb) {
     std::unordered_map<std::string, std::string> valueMap;
-
+    std::unordered_map<std::string, std::string> valuelabel;
     // First pass: collect and record constant assignments.
     for (auto &inst : bb->CoInst) {
-        if (inst.op == "li" && inst.fields.size() == 1) { // Assuming 'li' is the instruction to load immediate constants.
-            valueMap[inst.fields[1]] = inst.fields[0]; // Store constant value with its register.
-        }
+        if (inst.op == "li" && inst.fields.size() == 2) { // Assuming 'li' is the instruction to load immediate constants.
+            if (valueMap.find(inst.fields[0]) == valueMap.end()){
+                valueMap[inst.fields[0]] = inst.fields[1]; // Store constant value with its register.
+                valuelabel[inst.fields[0]] = "1";
+            }
+            else {
+                valuelabel[inst.fields[0]] = "2";
+            }
+        }  
     }
 
     // Second pass: substitute known constants.
@@ -33,9 +39,9 @@ void ConstProp::constantPropagation(sysy::BasicBlock *bb) {
     while (changed) {
         changed = false;
         for (auto &inst : bb->CoInst) {
-            if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[0]) != valueMap.end()) {
+            if (inst.valid && !inst.fields.empty() && valueMap.find(inst.fields[0]) != valueMap.end() && valuelabel[inst.fields[0]] == "1") {
                 // Replace uses of constants.
-                if (inst.op != "li") {
+                if (inst.op != "li" ) {
                     inst.fields[0] = valueMap[inst.fields[0]];
                     changed = true;
                 }
